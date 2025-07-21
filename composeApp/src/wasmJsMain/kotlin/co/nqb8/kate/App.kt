93d931fcf5f4a01b9eca284f4eea41b5a4b93025
Ayodele Kehinde <ayodelekehinde8@yahoo.com>
2025-07-21 15:26:27 +0100

    Add initial loading

--- composeApp/src/wasmJsMain/kotlin/co/nqb8/kate/App.kt
+++ composeApp/src/wasmJsMain/kotlin/co/nqb8/kate/App.kt
@@ -1,12 +1,18 @@
 package co.nqb8.kate
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import co.nqb8.kate.auth.LoginScreen
 import co.nqb8.kate.auth.TokenStorage
 import co.nqb8.kate.theme.KateTheme
+import kotlinx.browser.document
 
 @Composable
 fun App() {
+    LaunchedEffect(Unit){
+        val ele = document.getElementById("loading-indicator")
+        ele?.remove()
+    }
     KateTheme {
         if (TokenStorage.token.isEmpty()){
             LoginScreen()
